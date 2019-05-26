ed3a1eebad492e600c85c3e1916c9fd35f0829a8
Kamil <kamilcha@gmail.com>
2019-05-26 23:53:18 +0200

    Fix 'apply' and 'unapply' errors

--- sklep/app/models/ProductDetails.scala
+++ sklep/app/models/ProductDetails.scala
@@ -2,9 +2,10 @@ package models
 
 import play.api.libs.json._
 
-class ProductDetails(pd_id: Long, product_id: Long, full_desc: String)
+case class ProductDetails(pd_id: Long, product_id: Long, full_desc: String)
 
 object ProductDetails {
   implicit val productDetailsFormat = Json.format[ProductDetails]
-
 }
+
+
--- sklep/app/models/User.scala
+++ sklep/app/models/User.scala
@@ -4,11 +4,14 @@ import java.sql.Timestamp
 
 import play.api.libs.json._
 
-class User (user_id: Long, email_address: String, display_name:String, password: String,
+case class User (user_id: Long, email_address: String, display_name:String, password: String,
             first_name: String, last_name: String, address_1: String, address_2: String,
             phone_number: String, verified:Boolean, password_reset: Boolean, created_date: Timestamp,
             modified_date: Timestamp, payment_info: String)
 
 object User {
+  /** Overwrite the default Reads and Writes for Timestamp*/
+  implicit val tsreads: Reads[Timestamp] = Reads.of[Long] map (new Timestamp(_))
+  implicit val tswrites: Writes[Timestamp] = Writes { (ts: Timestamp) => JsString(ts.toString)}
   implicit val userFormat = Json.format[User]
 }
